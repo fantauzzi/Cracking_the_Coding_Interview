1f9c9654862d129811077709fb3bdcbd2472ddf1
Francesco Fantauzzi <Francesco_Fantauzzi@yahoo.com>
2017-12-30 17:10:46 +0100

    Added more compact solution for the rolling median exercise

--- heaps_find_the_running_meadian.py
+++ heaps_find_the_running_meadian.py
@@ -5,11 +5,11 @@ import heapq
 
 class Rolling_median_finder():
     def __init__(self):
-        self.heap_left = []
-        self.heap_right = []
-        self.median = None
+        self.heap_left = []  # It will be used as max-heap by reversing the sign of numbers added to it
+        self.heap_right = []  # A min-heap
+        self.median = None  # Only used in roll2() to keep track of the latest computed median
 
-    def roll(self, item):
+    def roll2(self, item):
         # Initialize the median for step 0 of the algorithm
         if self.median is None:
             self.median = item
@@ -31,6 +31,32 @@ class Rolling_median_finder():
         else:
             self.median = -self.heap_left[0] if len(self.heap_left) > len(self.heap_right) else self.heap_right[0]
         return self.median
+
+    # A more compact solution, that doesn't need to keep track of the last median computed
+    def roll(self, item):
+        # Push new item to the left heap (max-heap)
+        heapq.heappush(self.heap_left, -item)
+        # If the total number of items between the two heaps is even...
+        if (len(self.heap_left) + len(self.heap_right))  % 2 == 0:
+            # ... then pop the head from the left heap and add it to the right heap
+            popped = heapq.heappop(self.heap_left)
+            heapq.heappush(self.heap_right, -popped)
+        else:
+            # If instead it is odd...
+            if len(self.heap_right) > 0 and -self.heap_left[0] > self.heap_right[0]:
+                # ...then pop the head from each heap, and add it to the other one
+                left_head = heapq.heappop(self.heap_left)
+                right_head = heapq.heappop(self.heap_right)
+                heapq.heappush(self.heap_right, -left_head)
+                heapq.heappush(self.heap_left, -right_head)
+
+        # Determine and return the median
+        if (len(self.heap_left) + len(self.heap_right)) % 2 == 0:
+            # If the total number of items is even, the median is the average between the two heap heads
+            return (self.heap_right[0] -self.heap_left[0]) / 2
+        else:
+            # Otherwise it is the head of the left heap
+            return -self.heap_left[0]
 
 
 if __name__ == '__main__':
