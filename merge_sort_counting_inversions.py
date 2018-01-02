ebfbc5bf9ed6c99116765e8677314d50b779e744
Francesco Fantauzzi <Francesco_Fantauzzi@yahoo.com>
2018-01-02 13:05:23 +0100

    Adding first implementation for Merge Sort, Counting Inversions exercise, not performing enough

---
+++ merge_sort_counting_inversions.py
@@ -0,0 +1,70 @@
+#!/bin/python3
+
+import sys
+
+def count_inversions_bf(arr):
+    # Complete this function
+    if len(arr) <= 1:
+        return 0
+    total = 0
+    for i in range(len(arr)-1):
+        for item in arr[i+1:]:
+            if arr[i] > item:
+                total += 1
+
+    return total
+
+
+def count_inversions_between(left, right):
+    if len(left) == 0 or len(right) == 0:
+        return 0
+    i_r = 0
+    res = 0
+    for item in left:
+        res += i_r
+        while i_r < len(right) and item > right[i_r]:
+            res += 1
+            i_r += 1
+    return res
+
+
+def merge_sorted(left, right):
+    i_l, i_r = 0, 0
+    sorted = []
+    while i_l < len(left) and i_r < len(right):
+        if left[i_l] <= right[i_r]:
+            sorted.append(left[i_l])
+            i_l += 1
+        else:
+            sorted.append(right[i_r])
+            i_r += 1
+    if i_l < len(left):
+        sorted.extend(left[i_l:])
+    elif i_r < len(right):
+        sorted.extend(right[i_r:])
+    return sorted
+
+
+def count_and_sort(arr):
+    assert len(arr) > 0
+    if len(arr) == 1:
+        return 0, arr
+    elif len(arr) == 2:
+        return (0, arr) if arr[0] <= arr[1] else (1, [arr[1]] + [arr[0]])
+    arr_l = arr[0: len(arr) // 2]
+    arr_r = arr[len(arr) // 2:]
+    assert -1 <= len(arr_l) - len(arr_r) <= 1
+    l_count, l_sorted = count_and_sort(arr_l)
+    r_count, r_sorted = count_and_sort(arr_r)
+    sorted = merge_sorted(l_sorted, r_sorted)
+    straddling_count = count_inversions_between(l_sorted, r_sorted)
+    return l_count+r_count+straddling_count, sorted
+
+
+if __name__ == "__main__":
+    t = int(input().strip())
+    for a0 in range(t):
+        n = int(input().strip())
+        arr = list(map(int, input().strip().split(' ')))
+        result, _ = count_and_sort(arr)
+        print(result)
---
+++ test_merge_sort_counting_inversions.py
@@ -0,0 +1,61 @@
+from merge_sort_counting_inversions import count_inversions_between, merge_sorted, count_inversions_bf, count_and_sort
+
+
+def test_two_fingers():
+    a = [1, 3, 5, 6]
+    b = [2, 4, 6, 8]
+    assert merge_sorted(a, b) == [1, 2, 3, 4, 5, 6, 6, 8]
+
+
+def test_count_inversions_small():
+    a = [0, 2]
+    b = [1, 3]
+    assert count_inversions_between(a, b) == 1
+
+
+def test_count_inversions():
+    a = [1, 3, 4, 6]
+    b = [1, 2, 3, 3]
+    assert count_inversions_between(a, b) == 10
+
+
+def test_small_count_and_sort():
+    a = [0, 2, 1, 3]
+    count, _ = count_and_sort(a)
+    assert 1 == count
+
+
+def test_none():
+    a = list(range(10))
+    assert count_inversions_bf(a) == 0
+
+
+def test_single():
+    a = [0, 1, 3, 5, 4, 6, 7, 8]
+    assert count_inversions_bf(a) == 1
+
+
+def test_multiple():
+    a = [2, 1, 3, 1, 2]
+    assert count_inversions_bf(a) == 4
+    res, _ = count_and_sort(a)
+    assert res == 4
+
+
+def test_all():
+    a = list(range(9, -1, -1))
+    assert count_inversions_bf(a) == sum(a)
+    res, _ = count_and_sort(a)
+    assert res == sum(a)
+
+
+def test_load():
+    a = list(range(int(1e6)))
+    res, sorted = count_and_sort(a)
+    assert res == 0
+
+def test_load_reverse():
+    a = list(range(int(1e6), -1, -1))
+    res, sorted = count_and_sort(a)
+    assert res == sum(a)
+
